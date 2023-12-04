90d18a733115ac2548a1ea2f34550c768fca4371
Christian Thiago <christian.santosmlk@hotmail.com>
2023-12-04 15:53:24 -0300

    treino variaveis

---
+++ variaveis/script.js
@@ -0,0 +1,23 @@
+var nome = "Christian";
+var idade = 22;
+var possuefaculdade = true;
+
+console.log(nome, idade, possuefaculdade);
+
+var preco = 29;
+var totalComprado = 5;
+var totalComprado = totalComprado * preco;
+
+console.log(totalComprado);
+
+var sobrenome = "Christian",
+  cidade = "São Paulo";
+
+console.log(sobrenome, cidade);
+
+var comida = "strogonoff",
+  nota = 10,
+  adicionais = "Batata Palha",
+  bebida = "Coca Cola";
+
+console.log(comida, nota, adicionais, bebida);
