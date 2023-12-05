80e38b3d013799d3753542612cbf83e331cdec04
Christian Thiago <christian.santosmlk@hotmail.com>
2023-12-05 11:16:51 -0300

    final do treino de dados

--- tipo-de-dados/script.js
+++ tipo-de-dados/script.js
@@ -1,2 +1,32 @@
 var nome = "Christian";
+var idade = 22;
+var time = null;
 console.log(typeof nome);
+
+var nome = "Christian";
+var sobrenome = "Thiago";
+var nomeCompleto = nome + " " + sobrenome;
+console.log(nomeCompleto);
+
+var gols = 1000;
+var frase1 = "Romário fez " + gols + " gols";
+var frase2 = `Romário fez ${gols} gols`;
+
+console.log(frase2);
+
+// Declare uma variável contendo uma string
+var nomejog = "Neymar";
+console.log(typeof nomejog);
+// Declare uma variável contendo um número dentro de uma string
+var numero = 10;
+console.log(typeof numero);
+// Declare uma variável com a sua idade
+var idade = "31";
+console.log(idade);
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+var meunome = "christian";
+var meusobrenome = "thiago";
+console.log(meunome + " " + meusobrenome);
+// Coloque a seguinte frase em uma variável: It's time
+console.log(`It's time`);
